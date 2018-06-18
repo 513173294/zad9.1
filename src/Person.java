a1567dfad0503b6a0ebc490b35087ad490df29ae
Mateusz Barzantny <mateusz.barzantny@op.pl>
2018-06-18 20:25:01 +0200

    rzucanie wyjatkow z konstruktora

--- src/Person.java
+++ src/Person.java
@@ -4,14 +4,17 @@ public class Person {
     int age;
     int pesel;
 
-    public Person(String firstName, String lasttName, int age, int pesel) {
+    public Person(String firstName, String lasttName, int age, int pesel) throws NameUndefinedException, IncorrectAgeException {
+        if (firstName.length() < 2 || firstName == null) throw new NameUndefinedException();
+        if (lasttName.length() < 2 || lasttName == null) throw new NameUndefinedException();
+        if (age<1)throw new IncorrectAgeException();
         this.firstName = firstName;
         this.lasttName = lasttName;
         this.age = age;
         this.pesel = pesel;
     }
 
-      @Override
+    @Override
     public String toString() {
         return firstName + ", " + lasttName + ", " + age + ", " + pesel;
     }
--- src/Test.java
+++ src/Test.java
@@ -4,52 +4,75 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
 
-//
-
-        Scanner scanner = new Scanner(System.in);
         Person[] people = new Person[1];
-
-
-        int i = 0;
-        int age = 0;
-        String name = null;
-        String surName = null;
-        System.out.println("podaj imie: ");
-        while (i < people.length)
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < people.length; i++) {
+            System.out.println("podaj imnie");
+            String name = scanner.nextLine();
+            System.out.println("podaj nazwisko");
+            String surName = scanner.nextLine();
+            System.out.println("podaj wiek");
+            int age = scanner.nextInt();
+            System.out.println("podaj pesel");
+            int pesel = scanner.nextInt();
 
             try {
-                name = scanner.nextLine();
-                if (name.length() < 2 || name == null) throw new NameUndefinedException();
-
+                Person person = new Person(name, surName, age, pesel);
+                System.out.println(person);
             } catch (NameUndefinedException e) {
-                System.out.println("Podales za krotkie imie");
-            i=0;
-                break;
+                System.out.println("podaj przynajmniej 2 znaki");
+                i = 0;
+            } catch (IncorrectAgeException e) {
+                System.out.println("podales za niski wiek");
+                i = 0;
             }
 
-        try {
-            System.out.println("podaj nazwisko");
-            surName = scanner.nextLine();
-            if (surName.length() < 2 || surName == null) throw new NameUndefinedException();
-        } catch (NameUndefinedException e) {
-            System.out.println("podales za krotkie nazwisko");
-
-
         }
-        System.out.println("podaj wiek");
-        try {
-            age = scanner.nextInt();
-            if (age < 1) throw new IncorrectAgeException();
-        } catch (IncorrectAgeException ex) {
-            System.out.println("podales za niski wiek");
-
-        }
-        System.out.println("podaj pesel");
-        int pesel = scanner.nextInt();
-        Person person = new Person(name, surName, age, pesel);
-        people[i] = person;
-        System.out.println(person);
-
-i++;
     }
 }
+
+//
+//
+//        int i = 0;
+//        int age = 0;
+//        String name = null;
+//        String surName = null;
+//        System.out.println("podaj imie: ");
+//        while (i < people.length)
+//
+//            try {
+//                name = scanner.nextLine();
+//                if (name.length() < 2 || name == null) throw new NameUndefinedException();
+//
+//            } catch (NameUndefinedException e) {
+//                System.out.println("Podales za krotkie imie");
+//                i = 0;
+//                break;
+//            }
+//
+//        try {
+//            System.out.println("podaj nazwisko");
+//            surName = scanner.nextLine();
+//            if (surName.length() < 2 || surName == null) throw new NameUndefinedException();
+//        } catch (NameUndefinedException e) {
+//            System.out.println("podales za krotkie nazwisko");
+//
+//
+//        }
+//        System.out.println("podaj wiek");
+//        try {
+//            age = scanner.nextInt();
+//            if (age < 1) throw new IncorrectAgeException();
+//        } catch (IncorrectAgeException ex) {
+//            System.out.println("podales za niski wiek");
+//
+//        }
+//        System.out.println("podaj pesel");
+//        int pesel = scanner.nextInt();
+//        Person person = new Person(name, surName, age, pesel);
+//        people[i] = person;
+//        System.out.println(person);
+//
+//        i++;
+//    }
+//}
